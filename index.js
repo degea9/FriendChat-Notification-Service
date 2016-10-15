a2a580f4e572f0b3a24320a60cb6a6d480f22fab
tuandang <degea9@gmail.com>
2016-10-15 10:24:18 +0700

    Add firsebase

--- index.js
+++ index.js
@@ -17,4 +17,45 @@ app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
 
+var firebase = require("firebase");
+firebase.initializeApp({
+  serviceAccount: "FriendChat-c9590ad56875.json",
+  databaseURL: "https://friendchat-260ce.firebaseio.com"
+});
+
+var db = firebase.database();
+var ref = db.ref("message");
+var ref1 = db.ref("test");
+
+// ref.push({
+//     name:"tuan",
+//     email:"degea9@gmail.com"
+// })
+//
+// ref.push({
+//     name:"truong",
+//     email:"truong@gmail.com"
+// })
+
+
+
+ref.once("value", function(snapshot) {
+  console.log(snapshot.val());
+});
+
+ref.on("child_changed", function(snapshot) {
+  var message = snapshot.val();
+  console.log("The updated message is " + message.message);
+  ref1.push({
+       name:"child_changed",
+      email:"child_changed@gmail.com"
+   })
+});
+
+ref.on("child_added",function (snapshot) {
+  var message = snapshot.val();
+  console.log("The new message  " + message.message);
+
+})
+
 
